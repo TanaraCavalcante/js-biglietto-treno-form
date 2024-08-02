6dfa93fd06e9bb437796f462775a89a3e8d46cf7
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-02 15:37:44 -0400

    calcolo

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,3 @@
-/* //Raccolta de dati della pag
-const messagePrice = document.getElementById('info')
-
---------------------------
-       RACCOLTA DATI
---------------------------
-//Chiedi all'utente quanti km vuole percorrere
-const totalKm = parseInt(prompt('Quanti KM vuoi percorrere?', 100));
-console.log(totalKm);
-
-//Chiedi all'utente la sua età
-const age = parseInt(prompt('Quanti anni hai?', 31));
-console.log(age, typeof age);
-
----------------------------
-      FASI DI ELABORAZIONE
-----------------------------
-//Preparo della variabile PREZZO per Km
-const priceKm = 0.21;
-console.log (priceKm, typeof priceKm);
-
-//Prezzo del biglietto
-
-const ticketPrice = totalKm * priceKm;
-let message = "Il prezzo del tuo biglietto è € "+ (ticketPrice).toFixed(2);
-
-if (age < 18) {
-  const minAge = ticketPrice - (ticketPrice * 0.2);
-  message = "Il prezzo del tuo biglietto è € " + (minAge).toFixed(2);
-} else if (age > 65){
-  const maxAge = ticketPrice - (ticketPrice * 0.4);
-  message = "Il prezzo del tuo biglietto è € " + (maxAge).toFixed(2);
-} 
-
-console.log (message)
-
----------------------------
-      FASI DI OUTPUT
-----------------------------
-//Mostro in pagina
-messagePrice.innerHTML = (message)
-*/
-
-
 // TODO -------- RACCOLTA DATI -----------
 //input
 const inputName = document.getElementById('inputName');
@@ -54,13 +10,38 @@ const age = document.getElementById('selectElements');
 const btnGenerate = document.getElementById('generate-btn');
 const btnDel = document.getElementById('delet-btn');
 
+//Div - Messagio finale del biglieto
+const messageTicket = document.getElementById('info')
+
+//TODO -------------ELABORAZIONE------------------
+
+//Preparo della variabile PREZZO per Km
+const priceKm = 0.21;
+const rateName = '';
+console.log (priceKm);
+
+
+
 //TODO -------------GESTIONE EVENTI------------------
 //Raccolta dati
 btnGenerate.addEventListener('click', function(e){
     e.preventDefault();
     const inputNameValue = inputName.value.trim(); 
     const inputKmValue = parseInt(inputKm.value.trim());
-    console.log(inputNameValue, inputKmValue);
+    const ageValue = age.value;
+    console.log(inputNameValue, inputKmValue, ageValue);
+
+//-------------CALCOLO--------------------
+let finalPrice = inputKmValue * priceKm;
+
+if(ageValue === 'junior'){
+  finalPrice *=  0.8;
+  rateName = 'Hai avuto un sconto di 20%';
+}else if(ageValue === 'senior'){
+  finalPrice *= 0.6;
+  rateName = 'Hai avuto un sconto di 40%';
+}
+
 })
 
 btnDel.addEventListener('click', function(e){
@@ -69,21 +50,12 @@ btnDel.addEventListener('click', function(e){
   inputKm.value = ''; 
 })
 
-//TODO -------------ELABORAZIONE------------------
-//Text-Mensage
-const messagePrice = document.getElementById('info')
-const textMessage = "Il prezzo del tuo biglietto è € "
 
-//Preparo della variabile PREZZO per Km
-const priceKm = 0.21;
-console.log (priceKm);
 
-//TODO -------------CALCOLO-------------------------
 
-const inputKmValue = parseInt(inputKm.value.trim());
 
-//Prezzo del biglietto
-const ticketPrice 
+
+
 
 
 
